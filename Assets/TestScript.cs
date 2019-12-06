f91ad2128980db22f195173100e704a918f19ba9
LordPouic <janinloic@gmail.com>
2019-12-06 10:42:58 +0100

    4

---
+++ Assets/TestScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    void Update()
+    {
+        
+        transform.position += new Vector3(0.1f, 0.1f, 0.1f);
+    }
+}
